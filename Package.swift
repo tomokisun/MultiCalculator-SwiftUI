0c96596b2db4c1a9366b9d12f9021c0a6325a175
tomoki69386 <tomoki69386@users.noreply.github.com>
2021-06-14 17:31:40 +0000

    Run swift-format

--- Package.swift
+++ Package.swift
@@ -93,7 +93,7 @@ let package = Package(
     .target(
       name: "FeedbackGeneratorClient",
       dependencies: [
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
       ]
     ),
   ]
