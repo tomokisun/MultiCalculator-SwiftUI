cd6af3d6d4a19321e5c52fe047cf752cdea1548d
tomoki_sun <tomoki69386@gmail.com>
2021-06-15 01:48:01 +0900

    Add device state

---
+++ Sources/DeviceStateModifier/DeviceStateModifier.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+public struct DeviceStateModifier: ViewModifier {
+  @State var state = DeviceState.default
+  
+  public init() {}
+  
+  public func body(content: Content) -> some View {
+    content
+      .onAppear()
+      .onReceive(
+        NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
+      ) { _ in
+        state.previousOrientation = self.state.orientation
+        state.orientation = UIDevice.current.orientation
+      }
+      .environment(\.deviceState, state)
+  }
+}
+
+extension EnvironmentValues {
+  public var deviceState: DeviceState {
+    get { self[DeviceStateKey.self] }
+    set { self[DeviceStateKey.self] = newValue }
+  }
+}
+
+private struct DeviceStateKey: EnvironmentKey {
+  static var defaultValue: DeviceState {
+    .default
+  }
+}
